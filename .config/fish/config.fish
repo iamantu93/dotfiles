c75446369740ad6471f3779f470396dc89d1d716
Antu Acharjee <iam.antu93@gmail.com>
2019-05-13 21:25:15 +0600

    Updated I3 resize funcion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,30 +1,32 @@
 function fish_greeting
 end
 
+set -x PATH $PATH ~/.scripts/i3cmds/ ~/.scripts/tools/
 set -x TERMINAL st
 fish_vi_key_bindings
 
 function fish_prompt
-set_color red --bold
-printf "["
-set_color yellow
-printf $USER
-set_color green
-printf @
-set_color blue
-printf "$hostname "
-set_color magenta
-echo -n $PWD | sed 's|/home/antu|~|'
-set_color red
-printf "]"
-set_color green --bold
+	set_color red --bold
+	printf "["
+	set_color yellow
+	printf $USER
+	set_color green
+	printf @
+	set_color blue
+	printf "$hostname "
+	set_color magenta
+	echo -n $PWD | sed 's|/home/antu|~|'
+	set_color red
+	printf "]"
+	set_color green --bold
 
-if test -d (pwd)/.git
-printf "%s %s " (__fish_git_prompt) (__fish_git_prompt_informative_status)
-else
-printf " "
-end
-set_color normal
+	if test -d (pwd)/.git
+		printf "%s %s " (__fish_git_prompt) (__fish_git_prompt_informative_status)
+	else
+		printf " "
+	end
+	
+	set_color normal
 end
 
 abbr pi "sudo pacman -S"
