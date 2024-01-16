495c9b572b406be215efb5f5606d027673332baf
Antu Acharjee <35660520+iamantu93@users.noreply.github.com>
2024-01-16 13:54:29 +0600

    Create k8sreset.sh

---
+++ .scripts/k8sreset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+kubeadm reset -f
+rm -rf $HOME/.kube/config
+systemctl stop kubelet
+systemctl stop docker
+rm -rf /var/lib/cni/
+rm -rf /var/lib/kubelet/*
+rm -rf /etc/cni/
+ifconfig cni0 down
+ifconfig flannel.1 down
+ifconfig docker0 down
+ip link delete cni0
+ip link delete flannel.1
+iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
+systemctl start kubelet
+systemctl enable kubelet
+systemctl start docker
+systemctl enable docker
+kubeadm reset
+sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*
+sudo apt-get autoremove
+sudo rm -rf ~/.kube
